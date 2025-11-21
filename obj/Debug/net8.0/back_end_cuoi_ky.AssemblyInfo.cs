d1891177f60300a06a54ee63d9a717cde66de5f5
TanLoc <tanloc300185@gmail.com>
2025-11-21 14:35:31 +0700

    .

--- obj/Debug/net8.0/back_end_cuoi_ky.AssemblyInfo.cs
+++ obj/Debug/net8.0/back_end_cuoi_ky.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back_end_cuoi_ky")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d82928b9941563d0cdb84caac2ec4c672dfbf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab8d1f22c8cf79313b2b18d60eb01fd278d1ba9")]
 [assembly: System.Reflection.AssemblyProductAttribute("back_end_cuoi_ky")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back_end_cuoi_ky")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
