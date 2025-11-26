03ba4cc170014caf0bea4b5120ccbb7cf3d32a4f
Hieu-nguyen-lhu <hieunguyenlhu2005@gmail.com>
2025-11-26 15:38:01 +0700

    Push main

--- obj/Debug/net8.0/back_end_cuoi_ky.AssemblyInfo.cs
+++ obj/Debug/net8.0/back_end_cuoi_ky.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back_end_cuoi_ky")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ba6be4d906f2978657e6eecdc9575b97fc7868")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46253604fd23299df636a445eca8a8961a4991b")]
 [assembly: System.Reflection.AssemblyProductAttribute("back_end_cuoi_ky")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back_end_cuoi_ky")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
